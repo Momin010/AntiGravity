f81420f3380928e59e394f8a48317f3cf7e26ebc
Momin010 <momin.aldahdooh@gmail.com>
2025-11-19 08:53:05 +0200

    n

--- src/components/Dock/DockItem.jsx
+++ src/components/Dock/DockItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion, useTransform } from 'framer-motion';
-import { useOS } from '../context/OSContext';
+import { useOS } from '../../context/OSContext';
 
 const DockItem = ({ mouseX, app }) => {
     const { openWindow } = useOS();
